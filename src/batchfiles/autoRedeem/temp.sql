8b847c734319be950782231607ad2a2b048f24f9
Jonathan Cobb <jonna_cobb@yahoo.com>
2017-09-18 09:59:45 -0400

    Update report page with descriptions

--- src/batchfiles/autoRedeem/temp.sql
+++ src/batchfiles/autoRedeem/temp.sql
@@ -2,4 +2,4 @@ select p.*
 from developer.parcels_in_deposits2 p
 join billy.depvalld d on p.old_fig_deposits_record_id = d.id
 where d.deposit_date>='2017-03-20'
-and p.old_fig_deposits_record_id in (28466);
+and p.old_fig_deposits_record_id in (28587);
